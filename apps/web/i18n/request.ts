059a0167b1c8696d17e929c7b2a18e28eea96a3d
maxentr <rouaultmaxent@gmail.com>
2025-04-17 21:10:38 +0200

    fix(web): readd english as fallback language when translation is missing

--- apps/web/i18n/request.ts
+++ apps/web/i18n/request.ts
@@ -1,7 +1,7 @@
 import { routing } from "@/i18n/routing"
+import deepmerge from "deepmerge"
 import { hasLocale } from "next-intl"
 import { getRequestConfig } from "next-intl/server"
-
 export default getRequestConfig(async ({ requestLocale }) => {
   // Typically corresponds to the `[locale]` segment
   const requested = await requestLocale
@@ -9,8 +9,12 @@ export default getRequestConfig(async ({ requestLocale }) => {
     ? requested
     : routing.defaultLocale
 
+  const localeMessages = (await import(`../locales/${locale}.json`)).default
+  const defaultMessages = (await import(`../locales/en.json`)).default
+  const messages = deepmerge(defaultMessages, localeMessages)
+
   return {
     locale,
-    messages: (await import(`../locales/${locale}.json`)).default,
+    messages,
   }
 })
