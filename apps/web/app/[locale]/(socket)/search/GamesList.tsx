a25491844c218f66fa1c5db07da897f1f4ab4299
maxentr <rouaultmaxent@gmail.com>
2025-04-26 10:10:23 +0200

    style(web): update loading state UI for mobile in GameList

--- apps/web/app/[locale]/(socket)/search/GamesList.tsx
+++ apps/web/app/[locale]/(socket)/search/GamesList.tsx
@@ -150,7 +150,7 @@ const LoadingPublicGames = () => {
         exit={{ display: "none", transition: { duration: 0 } }}
       >
         <div className="flex flex-col gap-2">
-          <div className="flex flex-col sm:flex-row sm:items-center gap-0 sm:gap-2">
+          <div className="flex flex-col sm:flex-row sm:items-center gap-1 sm:gap-2">
             <div className="h-4 py-2 w-32 animate-pulse bg-gray-200 dark:bg-gray-600 rounded" />
             <div className="flex flex-row items-center gap-1">
               {Array.from({ length: 3 }).map((_, tagIndex) => (
