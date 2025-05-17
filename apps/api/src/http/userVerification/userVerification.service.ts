e3d1490fc82399a609ce088b8eee4d7dd0fdc4c1
maxentr <rouaultmaxent@gmail.com>
2025-05-17 09:47:42 +0200

    refactor(api): improve user verification logic and streamline PIN generation process

--- apps/api/src/http/userVerification/userVerification.service.ts
+++ apps/api/src/http/userVerification/userVerification.service.ts
@@ -1,32 +1,29 @@
 import { randomInt } from "crypto"
 import { db } from "@/db/index.js"
-import { userTable, userVerificationTable } from "@/db/schema.js"
+import { type UserDb, userTable, userVerificationTable } from "@/db/schema.js"
 import { mailerQueue } from "@/utils/mailer.js"
 import { Logger } from "@skymo/logger"
-import { UserError } from "@skymo/shared/constants"
-import { and, eq, lt } from "drizzle-orm"
+import { and, eq } from "drizzle-orm"
 
 export async function sendVerifyPin(email: string) {
-  const user = await db
+  const userResult = await db
     .select()
     .from(userTable)
-    .where(eq(userTable.email, email))
+    .where(and(eq(userTable.email, email), eq(userTable.emailVerified, false)))
     .limit(1)
 
-  // If the user does not exist, do nothing. This prevents giving away information about whether an email is registered or not.
-  if (user.length === 0) {
-    Logger.error(
-      "User not found when sending verify pin. This shouldn't happen since the function is behind authMiddleware.",
-      {
-        email,
-      },
-    )
-    throw new Error(UserError.UNEXPECTED_ERROR)
+  const user = userResult?.[0]
+  if (!user) {
+    Logger.warn("User not found or already verified", {
+      email,
+    })
+
+    return
   }
 
-  const locale = user[0].locale
+  const locale = user.locale
 
-  const pin = await generateVerifyPin(email)
+  const pin = await generateVerifyPin(user)
 
   await mailerQueue.add("verify-pin", {
     to: email,
@@ -38,23 +35,30 @@ export async function sendVerifyPin(email: string) {
   })
 }
 
-export async function generateVerifyPin(email: string) {
-  const user = await db
-    .select()
-    .from(userTable)
-    .where(eq(userTable.email, email))
-    .limit(1)
-
-  if (user.length === 0) {
-    throw new Error(UserError.NOT_FOUND)
-  }
-
+export async function generateVerifyPin(user: UserDb) {
   const pin = randomInt(0, 1000000).toString().padStart(6, "0")
   // 10 minutes
   const expiresAt = new Date(Date.now() + 1000 * 60 * 10)
+
+  const existingPinResult = await db
+    .select()
+    .from(userVerificationTable)
+    .where(eq(userVerificationTable.userId, user.id))
+    .limit(1)
+
+  const existingPin = existingPinResult?.[0]
+  if (existingPin) {
+    await db
+      .update(userVerificationTable)
+      .set({ pin, expiresAt })
+      .where(eq(userVerificationTable.id, existingPin.id))
+
+    return pin
+  }
+
   await db.insert(userVerificationTable).values({
     pin,
-    userId: user[0].id,
+    userId: user.id,
     expiresAt,
   })
 
@@ -62,30 +66,32 @@ export async function generateVerifyPin(email: string) {
 }
 
 export async function verifyPin(email: string, pin: string) {
-  const result = await db
+  const [result] = await db
     .select()
     .from(userVerificationTable)
     .innerJoin(userTable, eq(userVerificationTable.userId, userTable.id))
-    .where(
-      and(
-        eq(userVerificationTable.pin, pin),
-        eq(userTable.email, email),
-        lt(userVerificationTable.expiresAt, new Date()),
-      ),
-    )
+    .where(and(eq(userVerificationTable.pin, pin), eq(userTable.email, email)))
     .limit(1)
 
-  const user = result?.[0]
-  if (!user) return false
+  if (!result) return false
+
+  const { users: user, user_verifications } = result
+
+  // If the pin has expired, generate a new one and return unsuccessful
+  if (user_verifications.expiresAt < new Date()) {
+    await generateVerifyPin(user)
+
+    return false
+  }
 
   await db
     .update(userTable)
     .set({ emailVerified: true })
-    .where(eq(userTable.id, user.user_verifications.userId))
+    .where(eq(userTable.id, user_verifications.userId))
 
   await db
     .delete(userVerificationTable)
-    .where(eq(userVerificationTable.id, user.user_verifications.id))
+    .where(eq(userVerificationTable.id, user_verifications.id))
 
   return true
 }
