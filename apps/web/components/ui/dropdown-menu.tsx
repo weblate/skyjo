127ee07736ca0caee560b030e8c9274666cf329c
maxentr <rouaultmaxent@gmail.com>
2025-05-27 20:02:23 +0200

    style(web): update DropdownMenuSeparator background color for improved visual consistency

--- apps/web/components/ui/dropdown-menu.tsx
+++ apps/web/components/ui/dropdown-menu.tsx
@@ -162,7 +162,7 @@ const DropdownMenuSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 my-1 h-px bg-slate-100", className)}
+    className={cn("-mx-1 my-1 h-px bg-black/5", className)}
     {...props}
   />
 ))
