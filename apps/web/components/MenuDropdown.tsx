0a0fb749efe72e5dc19f03ffe1c04b20531dd2d1
maxentr <rouaultmaxent@gmail.com>
2025-05-27 20:03:51 +0200

    feat(web): enhance MenuDropdown with user profile and authentication options

--- apps/web/components/MenuDropdown.tsx
+++ apps/web/components/MenuDropdown.tsx
@@ -1,21 +1,26 @@
 "use client"
-
 import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuGroup,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { useFeedback } from "@/contexts/FeedbackContext"
 import { useRules } from "@/contexts/RulesContext"
 import { useSettings } from "@/contexts/SettingsContext"
+import { useAuth } from "@/hooks/useAuth"
+import { Link } from "@/i18n/routing"
 import {
   BookOpenIcon,
+  LogInIcon,
+  LogOutIcon,
   MenuIcon,
   MessageSquareWarningIcon,
   SettingsIcon,
-  SquareArrowOutUpRightIcon,
+  UserIcon,
 } from "lucide-react"
 import { useTranslations } from "next-intl"
 import Image from "next/image"
@@ -25,6 +30,7 @@ const MenuDropdown = () => {
   const { openRules } = useRules()
   const { openSettings } = useSettings()
   const t = useTranslations("components.MenuDropdown")
+  const { user, logout } = useAuth()
 
   return (
     <DropdownMenu>
@@ -33,22 +39,49 @@ const MenuDropdown = () => {
           <MenuIcon />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" side="top" sideOffset={8}>
-        <DropdownMenuItem onClick={openRules}>
-          <BookOpenIcon className="mr-2 h-4 w-4" />
-          <span>{t("rules")}</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={openFeedback}>
-          <MessageSquareWarningIcon className="mr-2 h-4 w-4" />
-          <span>{t("feedback")}</span>
-        </DropdownMenuItem>
-        <DropdownMenuItem>
-          <a
-            href="https://discord.gg/uSmGjhzZAE"
-            target="_blank"
-            className="flex items-center justify-between w-full"
-          >
-            <div className="flex items-center">
+      <DropdownMenuContent
+        className="w-52"
+        side="top"
+        sideOffset={8}
+        align="end"
+      >
+        <DropdownMenuGroup>
+          <div className="flex flex-col items-start px-2 py-1.5">
+            <span className="text-sm font-medium">
+              {user?.name ?? t("guest")}
+            </span>
+
+            {user && (
+              <span className="text-sm text-gray-500">@{user.username}</span>
+            )}
+          </div>
+          <DropdownMenuSeparator />
+          {user && (
+            <DropdownMenuItem asChild>
+              <Link href="/profile" className="flex items-center">
+                <UserIcon className="mr-2 size-4" />
+                <span>{t("profile")}</span>
+              </Link>
+            </DropdownMenuItem>
+          )}
+
+          <DropdownMenuItem onClick={openRules}>
+            <BookOpenIcon className="mr-2 size-4" />
+            <span>{t("rules")}</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={openSettings}>
+            <SettingsIcon className="mr-2 size-4" />
+            <span>{t("settings")}</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <a
+              href="https://discord.gg/uSmGjhzZAE"
+              target="_blank"
+              className="flex flex-row items-center mr-2"
+            >
               <Image
                 src="/svg/discord-outline.svg"
                 width={16}
@@ -57,14 +90,32 @@ const MenuDropdown = () => {
                 className="dark:invert mr-2"
               />
               <span>{t("discord")}</span>
-            </div>
-            <SquareArrowOutUpRightIcon className="size-3.5 opacity-50" />
-          </a>
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={openSettings}>
-          <SettingsIcon className="mr-2 h-4 w-4" />
-          <span>{t("settings")}</span>
-        </DropdownMenuItem>
+            </a>
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={openFeedback}>
+            <MessageSquareWarningIcon className="mr-2 size-4" />
+            <span>{t("feedback")}</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          {user ? (
+            <DropdownMenuItem>
+              <LogOutIcon className="mr-2 size-4" />
+              <button onClick={() => logout()}>{t("logout")}</button>
+            </DropdownMenuItem>
+          ) : (
+            <DropdownMenuItem asChild>
+              <Link
+                href="/login"
+                className="w-full flex items-center justify-start"
+              >
+                <LogInIcon className="mr-2 size-4" />
+                {t("login")}
+              </Link>
+            </DropdownMenuItem>
+          )}
+        </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   )
