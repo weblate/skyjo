8c248cfb0b0dcb2abf0d6c1125202f4dd1930ed9
maxentr <rouaultmaxent@gmail.com>
2025-05-30 21:51:48 +0200

    style(web): update DropdownMenuSeparator

--- apps/web/components/ui/dropdown-menu.tsx
+++ apps/web/components/ui/dropdown-menu.tsx
@@ -162,7 +162,7 @@ const DropdownMenuSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 my-1 h-px bg-black/5", className)}
+    className={cn("-mx-1 my-1 h-px bg-black/50 dark:bg-gray-500", className)}
     {...props}
   />
 ))
