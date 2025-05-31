4b96c08bcbd5b451dc3e4308460b762288d9939b
maxentr <rouaultmaxent@gmail.com>
2025-05-31 16:45:23 +0200

    style(web): update SearchLayout component by changing fragment to div for improved structure and layout consistency

--- apps/web/app/[locale]/(socket)/search/layout.tsx
+++ apps/web/app/[locale]/(socket)/search/layout.tsx
@@ -50,16 +50,16 @@ export async function generateMetadata(props: SearchLayoutProps) {
 
 export default async function SearchLayout({ children }: SearchLayoutProps) {
   return (
-    <>
+    <div className="flex flex-col">
       <Navbar className="mt-4" />
       <div className="relative min-h-svh w-full z-20 flex flex-col">
         <div className="w-full max-w-xl flex flex-grow self-center flex-col justify-center py-8 p-4">
-          <div className="bg-container dark:bg-dark-container border-2 border-black dark:border-dark-border rounded-2xl w-full flex flex-col gap-2 p-4 sm:p-8">
+          <div className="-translate-y-12 bg-container dark:bg-dark-container border-2 border-black dark:border-dark-border rounded-2xl w-full flex flex-col gap-2 p-4 sm:p-8">
             {children}
           </div>
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   )
 }
