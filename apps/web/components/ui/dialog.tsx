00a7b563ed672b5c14b9d2b867d777724ca1d997
maxentr <rouaultmaxent@gmail.com>
2025-07-06 15:58:13 +0200

    style(web): update Dialog component styles for improved layout and shadow effects

--- apps/web/components/ui/dialog.tsx
+++ apps/web/components/ui/dialog.tsx
@@ -47,7 +47,7 @@ const DialogContent = ({
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-2xl translate-x-[-50%] translate-y-[-50%] gap-4 border-[1.5px] border-black dark:border-dark-border bg-container dark:bg-dark-container text-black dark:text-dark-font p-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
+        "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-2xl translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border-[1.5px] border-black dark:border-dark-border bg-container dark:bg-dark-container text-black dark:text-dark-font p-6 shadow-lg duration-200",
         className,
       )}
       {...props}
