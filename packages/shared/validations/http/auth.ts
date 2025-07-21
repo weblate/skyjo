f6a2c668e8c81230eb53ef3d0549007c4be0e8f0
maxentr <rouaultmaxent@gmail.com>
2025-07-21 17:55:38 +0200

    fix(shared): enforce minimum length for login and password fields in login schema + trim them

--- packages/shared/validations/http/auth.ts
+++ packages/shared/validations/http/auth.ts
@@ -18,8 +18,8 @@ export type Signup = z.infer<typeof signupSchema>
 
 export const loginSchema = z.object({
   // login either email or username
-  login: z.string(),
-  password: z.string(),
+  login: z.string().trim().min(3),
+  password: z.string().trim().min(8),
 })
 export type LoginUser = z.infer<typeof loginSchema>
 
