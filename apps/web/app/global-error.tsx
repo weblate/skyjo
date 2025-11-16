ed016dc293a0f7ab475876d5a4078ac55124352e
maxentr <rouaultmaxent@gmail.com>
2025-11-16 10:35:09 +0100

    feat(web): capture global errors using PostHog for improved error tracking

--- apps/web/app/global-error.tsx
+++ apps/web/app/global-error.tsx
@@ -1,11 +1,17 @@
 "use client"
 
 import NextError from "next/error"
+import posthog from "posthog-js"
+import { useEffect } from "react"
 
 interface GlobalErrorProps {
   error: Error & { digest?: string }
 }
 export default function GlobalError({ error }: Readonly<GlobalErrorProps>) {
+  useEffect(() => {
+    posthog.captureException(error)
+  }, [error])
+
   return (
     <html lang="en">
       <body>
